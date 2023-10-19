71db3f19e72f238dea2c34499d7d9be60666c5f8
Maksim Filatau <maks.filatov.9494@gmail.com>
2023-10-19 17:34:11 +0200

    Start develop back, connection db

---
+++ Web_App/Rest/Authorization/Repositories/IUserRegistrationRepository.cs
@@ -0,0 +1,14 @@
+﻿using MySql.Data.MySqlClient;
+using Web_App.Rest.Authorization.Models;
+
+namespace Web_App.Rest.Authorization.Repositories
+{
+    public interface IUserRegistrationRepository
+    {
+        void addUserInDB(RegisterModel registerModel);
+        MySqlDataReader getByUsernameFromDB(string registerUsername);
+
+
+
+    }
+}
---
+++ Web_App/Rest/Authorization/Services/UserRegistrationService.cs
@@ -0,0 +1,45 @@
+﻿using MySql.Data.MySqlClient;
+using Web_App.Rest.Authorization.Models;
+using Web_App.Rest.Authorization.Repositories;
+
+namespace Web_App.Rest.Authorization.Services
+{
+    public class UserRegistrationService
+    {
+        private string _message = "";
+        private IUserRegistrationRepository _userRegistrationRepository;
+
+        public UserRegistrationService()
+        {
+            _userRegistrationRepository= new UserRegistrationRepository();
+        }
+
+
+        public bool chekUsername(RegisterModel userRegistModel)
+        {
+            MySqlDataReader reader = _userRegistrationRepository.getByUsernameFromDB(userRegistModel.Login);
+            if(reader.HasRows)
+            {
+                return true;
+            }
+            return false;
+        }
+
+
+
+
+
+
+
+
+        public string checkAllData(RegisterModel userRegistModel)
+        {
+            if (chekUsername(userRegistModel))
+                return _message = "Hujowy login";
+
+            return _message;
+        }
+
+
+    }
+}
