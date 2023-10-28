0c6adc4df313860f87b39b31f1f9e5ddbb0496b8
illbat1029 <illbat1029@gmail.com>
2023-10-28 21:45:48 +0200

    Simple mail sender via gmail

---
+++ Web_App/Rest/Authorization/Services/MailSendingService.cs
@@ -0,0 +1,32 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace Web_App.Rest.Authorization.Services
+{
+    public class MailSendingService
+    {
+        private readonly IConfiguration _configuration;
+        public MailSendingService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public void SendMailWithRecoveryLink(string email, string uid)
+        {
+            MailMessage message = new MailMessage();
+            string mailOrigin = "krwa.shop@gmail.com";
+            string mailAppKey = "xvbbqwbtfszivpbd";
+            message.From = new MailAddress(mailOrigin);
+            message.Subject = "Password Recovery Link";
+            message.To.Add(new MailAddress(email));
+            message.Body = ("<html><body>Your password recovery link is: https://localhost:44456/recoverypage/" + uid + " </body></html>");
+            message.IsBodyHtml = true;
+            var smtpClient = new SmtpClient("smtp.gmail.com")
+            {
+                Port = 587,
+                Credentials = new NetworkCredential(mailOrigin, mailAppKey),
+                EnableSsl = true,
+            };
+            smtpClient.Send(message);
+        }
+    }
+}
